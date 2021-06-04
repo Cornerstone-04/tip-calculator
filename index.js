5eb82b79ecdc95636ffc647cc6f919827070d517
Cornerstone <fortunecornerstone@gmail.com>
2021-06-04 10:38:50 +0100

    Update

--- index.js
+++ index.js
@@ -11,31 +11,25 @@ function Calc(){
    
     
     if (foodPrice.valueAsNumber=="" || foodTip.value=="" || numOfPeople.value=="") {
-        setTimeout(() => {
             document.querySelector('h2').textContent = "Please input all fields correctly.";
-            document.querySelector('h2').style.textAlign = "center"
-            setTimeout(() => {
-                header.textContent = "";
-            }, 1500)
-        }, 100)
-        
+            document.querySelector('h2').style.textAlign = "center";
       } else{
-        document.querySelector('.par1').innerHTML = "Tip:";
-        document.querySelector('.par5').innerHTML = "$" + tip;
+            document.querySelector('.par1').innerHTML = "Tip:";
+            document.querySelector('.par5').innerHTML = "$" + tip;
 
-        document.querySelector('.par2').innerHTML = "Total Amount:";
-        document.querySelector('.par6').innerHTML = "$" + totalAmount;
+            document.querySelector('.par2').innerHTML = "Total Amount:";
+            document.querySelector('.par6').innerHTML = "$" + totalAmount;
 
-        document.querySelector('.par3').innerHTML = "Tip Per Person:";
-        document.querySelector('.par7').innerHTML = "$" + tipPerPerson;
+            document.querySelector('.par3').innerHTML = "Tip Per Person:";
+            document.querySelector('.par7').innerHTML = "$" + tipPerPerson;
 
-        document.querySelector('.par4').innerHTML = "Total Per Person:";
-        document.querySelector('.par8').innerHTML = "$" + totalPerPerson;
+            document.querySelector('.par4').innerHTML = "Total Per Person:";
+            document.querySelector('.par8').innerHTML = "$" + totalPerPerson;
 
-        document.querySelector('h2').innerHTML = "RESULT";
-        document.querySelector('h2').style.backgroundColor = "#518428";
-        document.querySelector('h2').style.color = "#ffffff";
-        document.querySelector('h2').style.padding = "10px";
+            document.querySelector('h2').innerHTML = "RESULT";
+            document.querySelector('h2').style.backgroundColor = "#518428";
+            document.querySelector('h2').style.color = "#ffffff";
+            document.querySelector('h2').style.padding = "10px";
       }
 }
 
