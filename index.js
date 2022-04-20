0e3262226a0b8747ad49fcf8a141a72fe5003ba0
Cornerstone Ephraim <fortunecornerstone@gmail.com>
2022-04-20 15:18:58 +0100

    Adjusted index.css

--- index.js
+++ index.js
@@ -1,55 +1,59 @@
-const foodPrice = document.querySelector('.price');
-const foodTip = document.querySelector('.tip');
-const numOfPeople = document.querySelector('.Num');
-const clearAll = document.querySelector('.clearBtn');
+const foodPrice = document.querySelector(".price");
+const foodTip = document.querySelector(".tip");
+const numOfPeople = document.querySelector(".Num");
+const clearAll = document.querySelector(".clearBtn");
 
-function Calc(){
-    let tip = (foodPrice.valueAsNumber) * (foodTip.value / 100);
-    let totalAmount = (foodPrice.valueAsNumber) + (tip);
-    let tipPerPerson = tip / numOfPeople.value;
-    let totalPerPerson = totalAmount / numOfPeople.value;
-   
-    
-    if (foodPrice.valueAsNumber=="" || foodTip.value=="" || numOfPeople.value=="") {
-            document.querySelector('h2').textContent = "Please input all fields correctly.";
-            document.querySelector('h2').style.textAlign = "center";
-      } else{
-            document.querySelector('.par1').innerHTML = "Tip:";
-            document.querySelector('.par5').innerHTML = "$" + tip.toFixed(2);
+function Calc() {
+  let tip = foodPrice.valueAsNumber * (foodTip.value / 100);
+  let totalAmount = foodPrice.valueAsNumber + tip;
+  let tipPerPerson = tip / numOfPeople.value;
+  let totalPerPerson = totalAmount / numOfPeople.value;
 
-            document.querySelector('.par2').innerHTML = "Total Amount:";
-            document.querySelector('.par6').innerHTML = "$" + totalAmount.toFixed(2);
+  if (
+    foodPrice.valueAsNumber == "" ||
+    foodTip.value == "" ||
+    numOfPeople.value == ""
+  ) {
+    document.querySelector("h2").textContent =
+      "Please input all fields correctly.";
+    document.querySelector("h2").style.textAlign = "center";
+  } else {
+    document.querySelector(".par1").innerHTML = "Tip:";
+    document.querySelector(".par5").innerHTML = "$" + tip.toFixed(2);
 
-            document.querySelector('.par3').innerHTML = "Tip Per Person:";
-            document.querySelector('.par7').innerHTML = "$" + tipPerPerson.toFixed(2);
+    document.querySelector(".par2").innerHTML = "Total Amount:";
+    document.querySelector(".par6").innerHTML = "$" + totalAmount.toFixed(2);
 
-            document.querySelector('.par4').innerHTML = "Total Per Person:";
-            document.querySelector('.par8').innerHTML = "$" + totalPerPerson.toFixed(2);
+    document.querySelector(".par3").innerHTML = "Tip Per Person:";
+    document.querySelector(".par7").innerHTML = "$" + tipPerPerson.toFixed(2);
 
-            document.querySelector('h2').innerHTML = "RESULT";
-            document.querySelector('h2').style.backgroundColor = "#518428";
-            document.querySelector('h2').style.color = "#ffffff";
-            document.querySelector('h2').style.padding = "10px";
-      }
+    document.querySelector(".par4").innerHTML = "Total Per Person:";
+    document.querySelector(".par8").innerHTML = "$" + totalPerPerson.toFixed(2);
+
+    document.querySelector("h2").innerHTML = "RESULT";
+    document.querySelector("h2").style.backgroundColor = "#518428";
+    document.querySelector("h2").style.color = "#ffffff";
+    document.querySelector("h2").style.padding = "10px";
+  }
 }
 
-let inputs = document.querySelectorAll('input');
+let inputs = document.querySelectorAll("input");
 
-clearAll.addEventListener('click', () =>{
-    inputs.forEach(input => input.value = '');
+clearAll.addEventListener("click", () => {
+  inputs.forEach((input) => (input.value = ""));
 
-    document.querySelector('h2').innerHTML = '';
-    document.querySelector('h2').style = '';
+  document.querySelector("h2").innerHTML = "";
+  document.querySelector("h2").style = "";
 
-    document.querySelector('.par1').innerHTML = '';
-    document.querySelector('.par5').innerHTML = '';
+  document.querySelector(".par1").innerHTML = "";
+  document.querySelector(".par5").innerHTML = "";
 
-    document.querySelector('.par2').innerHTML = '';
-    document.querySelector('.par6').innerHTML = '';
+  document.querySelector(".par2").innerHTML = "";
+  document.querySelector(".par6").innerHTML = "";
 
-    document.querySelector('.par3').innerHTML = '';
-    document.querySelector('.par7').innerHTML = '';
-    
-    document.querySelector('.par4').innerHTML = '';
-    document.querySelector('.par8').innerHTML = '';
+  document.querySelector(".par3").innerHTML = "";
+  document.querySelector(".par7").innerHTML = "";
+
+  document.querySelector(".par4").innerHTML = "";
+  document.querySelector(".par8").innerHTML = "";
 });
